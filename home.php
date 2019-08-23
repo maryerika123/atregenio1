3ad9d53fcce0dd02430663130dbe9b0dee339d5e
maryerika123 <53499740+maryerika123@users.noreply.github.com>
2019-08-23 20:21:06 +0800

    Create home.php

---
+++ home.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+<center>
+<h1>AtregenioF photo Gallery<h1>
+<h2>List of Photos<h2>
+
+<nav>
+
+<a href="photo1.html">Photo 1</a> 
+<br>
+<a href="photo2.html">Photo 2</a>
+<br>
+<a href="photo3.html">Photo 3</a> 
+<br>
+
+
+
+</nav>
+</center>
+
+</body>
+
+</html>
